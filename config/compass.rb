0feff81eb1c453d90b28e7abb371dcf05f33c554
glaszig <glaszig@gmail.com>
2014-05-08 21:20:33 +0200

    compassified site, using sass, improved css, cleaned html

---
+++ config/compass.rb
@@ -0,0 +1,11 @@
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "_sass"
+css_dir = "assets/stylesheets"
+images_dir = "assets/images"
+fonts_dir = "assets/fonts"
+javascripts_dir = "assets/javascripts"
+line_comments = false
+preferred_syntax = :scss
+output_style = (environment == :production) ? :compressed : :expanded
+relative_assets = true
